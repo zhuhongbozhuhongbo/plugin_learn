9b710c98590147329a4702377b5fe25de5269399
GitHub Action <action@github.com>
2020-12-31 03:44:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zhuhongbozhuhongbo/pluginlearn/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zhuhongbozhuhongbo.pluginlearn.services
+
+import com.github.zhuhongbozhuhongbo.pluginlearn.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
